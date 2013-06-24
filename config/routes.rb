38a8cbefe13e564da3f8065340dae2ea751054bd
magicSwing <magic.cwin@gmail.com>
2013-06-24 16:28:06 +0800

    Added admin namespaced users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Ticketee::Application.routes.draw do
+
   devise_for :users
 
   # The priority is based upon order of creation:
@@ -49,6 +50,9 @@ Ticketee::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  namespace :admin do
+    resources :users
+  end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
@@ -59,4 +63,7 @@ Ticketee::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
+
+
 end
